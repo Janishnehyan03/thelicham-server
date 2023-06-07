3089f19f85add33f7d330f7f02d19b1857160243
Janishnehyan03 <nehyanjanish@gmail.com>
2023-06-07 23:06:23 +0530

    test

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -17,7 +17,7 @@ const handleCastError = (error, res) => {
 };
 
 const handleMongooseError = (error, res) => {
-  // console.log(error);
+  console.log(error);
   if (error.code === 11000) {
     handleDuplicateKeyError(error, res);
   } else if (error.name === "ValidationError") {
@@ -25,7 +25,6 @@ const handleMongooseError = (error, res) => {
   } else if (error.name === "CastError") {
     handleCastError(error, res);
   } else {
-    console.log(error);
     res.status(500).json({ message: "Internal Server Error", error });
   }
 };

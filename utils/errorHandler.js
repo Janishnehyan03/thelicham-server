37cd0374ff35cbea514852fe825bb380ed65de4a
Janishnehyan03 <nehyanjanish@gmail.com>
2023-06-07 23:10:02 +0530

    error testing

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -23,9 +23,11 @@ const handleMongooseError = (error, res) => {
   } else if (error.name === "ValidationError") {
     handleValidationError(error, res);
   } else if (error.name === "CastError") {
-    handleCastError(error, res);
-  } else {
     
+    handleCastError(error, res);
+
+  } else {
+
     res.status(500).json({ message: "Internal Server Error", error });
   }
 };
